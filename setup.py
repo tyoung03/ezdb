7ab606ddfcd05e2f3e2c238a5303bef428f0af87
tyoung03 <42500426+tyoung03@users.noreply.github.com>
2018-08-18 16:58:11 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='ezdb',
+  packages=['ezdb'],
+  version='1.0',
+  description='Access a remote database like you would a native python structure',
+  author='Tyler Young',
+  author_email='tyler.young@villanova.edu',
+  url='https://github.com/tyoung03/ezdb', 
+  download_url= 'https://github.com/tyoung03/ezdb/archive/master.zip',
+  keywords = ['database', 'ezdb', 'EZDB', 'easy', 'db', 'python'],
+  classifiers = [],
+)
