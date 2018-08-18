527b8f7d19620e2659002a4cb68b105b63b141f9
tyoung03 <42500426+tyoung03@users.noreply.github.com>
2018-08-18 18:36:11 -0400

    Delete server_example.py

--- server_example.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Api
-from ezdb import EZDBServer
-
-app = Flask(__name__)
-api = Api(app)
-api.add_resource(EZDBServer, '/<string:get_msg_unicode>')
-if __name__ == '__main__':
-    app.run(debug=True)
